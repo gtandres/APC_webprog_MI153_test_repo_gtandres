309049f1ca247af3222eab72499ced0319cda80f
gtandres <cloudaxx@gmail.com>
2016-12-16 00:19:28 -0800

    Added Line 3-9 in News.php

--- Exercise6/Codeigniter/application/controllers/News.php
+++ Exercise6/Codeigniter/application/controllers/News.php
@@ -1,3 +1,9 @@
 <?php
 class News extends CI_Controller {
-}
+
+ public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('news_model');
+        $this->load->helper('url_helper');
+    }
