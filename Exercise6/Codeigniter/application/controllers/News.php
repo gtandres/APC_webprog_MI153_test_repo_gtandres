7c0375bdb2b321a51d21ca4a13c4bcac7d0d1aa3
gtandres <cloudaxx@gmail.com>
2016-12-16 00:23:18 -0800

    Added lines 17-18

--- Exercise6/Codeigniter/application/controllers/News.php
+++ Exercise6/Codeigniter/application/controllers/News.php
@@ -14,5 +14,6 @@ class News extends CI_Controller {
 	
 	
 		$this->load->view('templates/header', $data);
-		
+		$this->load->view('news/index', $data);
+        $this->load->view('templates/footer');
 	}
