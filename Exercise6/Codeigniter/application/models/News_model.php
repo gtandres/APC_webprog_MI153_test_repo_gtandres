4935b6558af033769e034097880606ff30e90f62
gtandres <cloudaxx@gmail.com>
2016-12-16 00:53:34 -0800

    Created News_model.php

---
+++ Exercise6/Codeigniter/application/models/News_model.php
@@ -0,0 +1,19 @@
+<?php
+class News_model extends CI_Model {
+ 
+    public function __construct()
+    {
+        $this->load->database();
+    }
+    
+    public function get_news($User_ID = FALSE)
+    {
+        if ($User_ID === FALSE)
+        {
+            $query = $this->db->get('userinfo');
+            return $query->result_array();
+        }
+ 
+        $query = $this->db->get_where('userinfo', array('User_ID' => $User_ID));
+        return $query->row_array();
+    }
