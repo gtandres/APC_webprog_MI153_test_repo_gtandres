56b61ac7eaef6ea350a3e338f8cc047f57bcef18
gtandres <cloudaxx@gmail.com>
2016-12-21 21:05:29 -0800

    Generated userinfo

---
+++ Exercise7/basic/views/userinfo/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Userinfo */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="userinfo-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'n-ame')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'homeA')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'gender')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cellno')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'comment')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
