e296ebef298638f6294958a13aad4c4420bb32e5
gtandres <cloudaxx@gmail.com>
2016-12-21 20:56:32 -0800

    updated Trivia.php

--- Exercise7/basic/views/site/Trivia.php
+++ Exercise7/basic/views/site/Trivia.php
@@ -1,7 +1,7 @@
 <h2> Trivia! </h2>
 
 			<p>
-				1. What do I often do when I'm home?
+				1. What do you often do when you're home?
 			</p>
 
 			<p id="q1">
@@ -11,7 +11,7 @@
 			<button type="button" onclick="document.getElementById('q1').innerHTML = 'Video Games, Mix Music, Draw'">Click to see answer</button>
 			
 			<p>
-				2. What country do I want to visit the most?
+				2. What country do you want to visit the most?
 			</p>
 
 			<p id="q2">
@@ -31,24 +31,74 @@
 			<button type="button" onclick="document.getElementById('q3').innerHTML = 'Professional DJ, Character Designer, Game Analyst, Professional Basketball player'">Click to see answer</button>
 
 			<p>
-				4. What else do you do in your free time?
+				4. What do you find most satisfying?
 			</p>
 
 			<p id="q4">
 				ANSWER
 			</p>
 
-			<button type="button" onclick="document.getElementById('q4').innerHTML = 'Play on my PS3, or play basketball with friends.'">Click to see answer</button>
+			<button type="button" onclick="document.getElementById('q4').innerHTML = 'Winning'">Click to see answer</button>
 
 			<p>
-				5. Where do you go to buy magic cards?
+				5. Best Trait?
 			</p>
 
 			<p id="q5">
 				ANSWER
 			</p>
 
-			<button type="button" onclick="document.getElementById('q5').innerHTML = 'Hobby shops in taft or SM Megamall, or in Neutral grounds.'">Click to see answer</button>
+			<button type="button" onclick="document.getElementById('q5').innerHTML = 'Chill'">Click to see answer</button>
+			
+				<p>
+				1. Favorate Gaming company
+			</p>
+
+			<p id="q1">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q1').innerHTML = 'Square Enix'">Click to see answer</button>
+			
+			<p>
+				2. What country do you want to visit the most?
+			</p>
+
+			<p id="q2">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q2').innerHTML = 'JAPAN'">Click to see answer</button>
+
+			<p>
+				3. What other professions do you want?
+			</p>
+
+			<p id="q3">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q3').innerHTML = 'Professional DJ, Character Designer, Game Analyst, Professional Basketball player'">Click to see answer</button>
+
+			<p>
+				4. What do you find most satisfying?
+			</p>
+
+			<p id="q4">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q4').innerHTML = 'Winning'">Click to see answer</button>
+
+			<p>
+				5. Best Trait?
+			</p>
+
+			<p id="q5">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q5').innerHTML = 'Chill'">Click to see answer</button>
 
 
 			
