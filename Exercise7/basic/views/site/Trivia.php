fc902284600c59bd9ef806039262af5cfaba3dc3
gtandres <cloudaxx@gmail.com>
2016-12-21 20:47:41 -0800

    created Trivia.php

---
+++ Exercise7/basic/views/site/Trivia.php
@@ -0,0 +1,54 @@
+<h2> Trivia! </h2>
+
+			<p>
+				1. What do I often do when I'm home?
+			</p>
+
+			<p id="q1">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q1').innerHTML = 'Video Games, Mix Music, Draw'">Click to see answer</button>
+			
+			<p>
+				2. What country do I want to visit the most?
+			</p>
+
+			<p id="q2">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q2').innerHTML = 'JAPAN'">Click to see answer</button>
+
+			<p>
+				3. What other professions do you want?
+			</p>
+
+			<p id="q3">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q3').innerHTML = 'Professional DJ, Character Designer, Game Analyst, Professional Basketball player'">Click to see answer</button>
+
+			<p>
+				4. What else do you do in your free time?
+			</p>
+
+			<p id="q4">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q4').innerHTML = 'Play on my PS3, or play basketball with friends.'">Click to see answer</button>
+
+			<p>
+				5. Where do you go to buy magic cards?
+			</p>
+
+			<p id="q5">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q5').innerHTML = 'Hobby shops in taft or SM Megamall, or in Neutral grounds.'">Click to see answer</button>
+
+
+			
