b85b2f6b1546fc6a226e27ef906477b66e98505c
gtandres <cloudaxx@gmail.com>
2016-12-21 18:02:41 -0800

    Edited News.php

--- Exercise6/Codeigniter/application/controllers/News.php
+++ Exercise6/Codeigniter/application/controllers/News.php
@@ -1,53 +1,52 @@
 <?php
 class News extends CI_Controller {
-
- public function __construct()
+ 
+    public function __construct()
     {
         parent::__construct();
         $this->load->model('news_model');
         $this->load->helper('url_helper');
     }
-	  public function index()
+ 
+    public function index()
     {
         $data['userinfo'] = $this->news_model->get_news();
         $data['title'] = 'Website Visitors';
-	
-	
-		$this->load->view('templates/header', $data);
-		$this->load->view('news/index', $data);
+ 
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/index', $data);
         $this->load->view('templates/footer');
-	}
-	
-	 public function view($User_ID = NULL)
+    }
+ 
+    public function view($User_ID = NULL)
     {
         $data['news_item'] = $this->news_model->get_news($User_ID);
-		
-	 if (empty($data['news_item']))
+        
+        if (empty($data['news_item']))
         {
             show_404();
-	}
-	
-	  $this->load->view('templates/header', $data);
-      $this->load->view('news/view', $data);
-	  $this->load->view('templates/footer');
-	  }
-	  
-	   public function create()
+        }
+ 
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/view', $data);
+        $this->load->view('templates/footer');
+    }
+    
+    public function create()
     {
         $this->load->helper('form');
         $this->load->library('form_validation');
-		
-		  $data['title'] = 'Create a news item';
  
-        $this->form_validation->set_rules('Name', 'Name', 'required');
-        $this->form_validation->set_rules('Nickname', 'Nickname', 'required');
-		$this->form_validation->set_rules('Email', 'Email', 'required');
-		$this->form_validation->set_rules('Home_Address', 'Home address', 'required');
-		$this->form_validation->set_rules('Gender', 'Gender', 'required');
-		$this->form_validation->set_rules('CP_num', 'Cellphone number');
-		$this->form_validation->set_rules('Comment', 'Comment', 'required');
-		
-		
+        $data['title'] = 'Create a news item';
+ 
+        $this->form_validation->set_rules('name', 'name', 'required');
+        $this->form_validation->set_rules('nName', 'nName', 'required');
+		$this->form_validation->set_rules('email', 'email', 'required');
+        $this->form_validation->set_rules('hAd', 'hAd', 'required');
+		$this->form_validation->set_rules('gender', 'gender', 'required');
+        $this->form_validation->set_rules('cNum', 'cNum');
+		$this->form_validation->set_rules('message', 'comment', 'required');
+ 
         if ($this->form_validation->run() === FALSE)
         {
             $this->load->view('templates/header', $data);
@@ -55,17 +54,15 @@ class News extends CI_Controller {
             $this->load->view('templates/footer');
  
         }
-		
-		  else
+       else
         {
-            $this->news_model->set_news();
-            $this->load->view('templates/header', $data);
-            $this->load->view('news/success');
-            $this->load->view('templates/footer');
+            $this->news_model->set_news($User_ID);
+            //$this->load->view('news/success');
+            redirect( base_url() . 'index.php/news');
         }
     }
-	
-	 public function edit()
+    
+    public function edit()
     {
         $User_ID = $this->uri->segment(3);
         
@@ -73,39 +70,36 @@ class News extends CI_Controller {
         {
             show_404();
         }
-		
-		   $this->load->helper('form');
+        
+        $this->load->helper('form');
         $this->load->library('form_validation');
         
         $data['title'] = 'Edit a news item';        
         $data['news_item'] = $this->news_model->get_news_by_id($User_ID);
-		
-		 
-        $this->form_validation->set_rules('Name', 'Name', 'required');
-        $this->form_validation->set_rules('Nickname', 'Nickname', 'required');
-		$this->form_validation->set_rules('Email', 'Email', 'required');
-        $this->form_validation->set_rules('Home_Address', 'Home address', 'required');
-		$this->form_validation->set_rules('Gender', 'Gender', 'required');
-        $this->form_validation->set_rules('CP_num', 'Cellphone number');
-		$this->form_validation->set_rules('Comment', 'Comment', 'required');
-		
-		 if ($this->form_validation->run() === FALSE)
+        
+        $this->form_validation->set_rules('name', 'name', 'required');
+        $this->form_validation->set_rules('n_name', 'n_name', 'required');
+		$this->form_validation->set_rules('email', 'email', 'required');
+        $this->form_validation->set_rules('homeA', 'homeA', 'required');
+		$this->form_validation->set_rules('gender', 'gender', 'required');
+        $this->form_validation->set_rules('cellno', 'cellno');
+		$this->form_validation->set_rules('comment', 'comment', 'required');
+ 
+        if ($this->form_validation->run() === FALSE)
         {
             $this->load->view('templates/header', $data);
             $this->load->view('news/edit', $data);
             $this->load->view('templates/footer');
  
         }
-		
-		  else
+        else
         {
             $this->news_model->set_news($User_ID);
             //$this->load->view('news/success');
             redirect( base_url() . 'index.php/news');
         }
     }
-	
-	   
+    
     public function delete()
     {
         $User_ID = $this->uri->segment(3);
@@ -121,9 +115,5 @@ class News extends CI_Controller {
         redirect( base_url() . 'index.php/news');        
     }
 }
-
-		
-		
-		
 		
 		
