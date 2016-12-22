ac9839541950c5529aadf7097d46f4b461e1f889
gtandres <cloudaxx@gmail.com>
2016-12-21 18:43:10 -0800

    created views.php

---
+++ Exercise6/Codeigniter/application/views/News/View.php
@@ -0,0 +1,14 @@
+<?php
+echo $news_item['name'];
+echo "<br>";
+echo $news_item['n_name'];
+echo "<br>";
+echo $news_item['email'];
+echo "<br>";
+echo $news_item['homeA'];
+echo "<br>";
+echo $news_item['gender'];
+echo "<br>";
+echo $news_item['cellno'];
+echo "<br>";
+echo $news_item['comment'];
