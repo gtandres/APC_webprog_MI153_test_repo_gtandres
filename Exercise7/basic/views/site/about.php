499ff8c382e3846fdcb43a852b14a3b0a679bf97
gtandres <cloudaxx@gmail.com>
2016-12-21 20:22:12 -0800

    Edited about.php

--- Exercise7/basic/views/site/about.php
+++ Exercise7/basic/views/site/about.php
@@ -1,18 +1,56 @@
 <?php
-
 /* @var $this yii\web\View */
-
 use yii\helpers\Html;
-
-$this->title = 'About';
+$this->title = 'About the user';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+	<style>
+		table, th, td {
+			border: 4px solid black;
+			color: #0039e6;
+			height: 10%
+			width: 50%;
+		}
+	</style>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+			<table id="table" align="center">
+			<tr>
+				<th>Field</th>
+				<th>Information</th>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>Giomar Andres</td>
+			</tr>
+			<tr>
+				<td>Block</td>
+				<td>MI-153</td>
+			</tr>
+			<tr>
+				<td>Age:</td>
+				<td>17</td>
+			</tr>
+			<tr>
+				<td>Course:</td>
+				<td>BS Information Technology, Specializing in mobile and internet technology</td>
+			</tr>
+			<tr>
+			<td>HOBBIES</td>
+			<td>INTERESTS</td>
+			</tr>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
-</div>
+			<td>Video Games</td>
+			<td>PROGRAMMING </td>
+			</tr>
+			
+			<tr>
+			<td>MUSIC</td>
+			<td>BASKETBALL</td>
+			</tr>
+			
+			<tr>
+			<td>DRAWING</td>
+			<td>Disco Junkie (DJ) </td>
+			</tr>
+			</table>
+			
